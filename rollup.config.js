efbfbfe4efffe46b4543c516ad7251bf1b68beed
Mudit Ameta <zeusdeux@gmail.com>
2019-12-27 21:53:41 +0100

    Bundle only lib and minify it, etc

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
 
 export default [
   {
     input: './src/index.js',
-    output: {
-      file: 'dist/browser-gql.umd.js',
-      format: 'umd',
-      name: 'browserGql'
-    },
-    plugins: [replace({ 'process.env.NODE_ENV': '"production"' }), resolve(), commonjs()]
-  },
-  {
-    input: './src/demo.js',
-    output: {
-      file: 'dist/browser-gql-demo.umd.js',
-      format: 'umd',
-      name: 'browserGqlDemo'
-    },
-    plugins: [replace({ 'process.env.NODE_ENV': '"production"' }), resolve(), commonjs()]
+    output: [
+      {
+        file: 'dist/browser-gql.umd.js',
+        format: 'umd',
+        name: 'browserGql',
+        sourcemap: true
+      },
+      {
+        file: 'dist/browser-gql.esm.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [replace({ 'process.env.NODE_ENV': '"production"' }), resolve(), commonjs(), terser()]
   }
 ]
