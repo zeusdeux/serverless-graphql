d87ea0626ac183b4dd0174aacca1df7de5634b23
Mudit Ameta <zeusdeux@gmail.com>
2019-12-28 05:06:59 +0100

    Update and document the build process
    Current process would not work when using locally with apollo-server
    and it surfaced a lot of problems. These fixes _should_ solve them.
    
    Also, TIL that you cannot use npm link across different directories
    where one is the source and other is dependent when you have a peer
    dependency in your source directory. This only work if the dependent
    directory is nested somewhere in the source directory structure. This
    is because when you use npm link in the depedent directory and then
    node or your bundler tries to locate the peer dependency, the look up
    algorithm starts off in the source directory since that is where your
    "main" bundle is sitting. Even if your _dependent_ tree _has_ the
    peerDep, this look up can fail if you haven't installed the peerDep in
    the source tree as well.
    
    God that fucked me up for a bit.

--- Readme.md
+++ Readme.md
@@ -3,3 +3,38 @@
 Run GraphQL on any schema directly from anywhere without the need for a graphql server.
 
 This is _super_ experimental and just fun mostly.
+
+## Packages published
+
+This repo publishes a umd package with its external dependencies, `graphql` and `graphql/utilities`,
+all bundled up together as a UMD bundle for ease of usage from a browser.
+
+For usage from an application, it _does not_ bundle up any of its external dependencies (`graphql`
+and `graphql/utilities`). It instead marks `graphql` (which includes `graphql/utilities`) as a peer
+dependency under `peerDependencies`. It is up to the consumer of this package to install the
+`peerDependencies` in their application. This is because, at any given time, only one instance of
+`graphql` can be installed otherwise it starts throwing when it's functions are consumed.
+
+### UNPKG
+
+[UNPKG](https://unpkg.com/) serves `umd/serverless-gql.umd.js` which is minified and contains the
+dependencies within.
+
+### CJS
+
+Usage of this package using `require` or in CJS environments consumes `dist/serverless-gql.cjs.js`.
+
+### ESM
+
+In the context of an ESM based codebase, bundlers can choose to consume `dist/serverless-gql.esm.js`
+which is a build with `import`/`export` statements kept intact for better tree-shaking to enable
+dead code elimination.
+
+## Development
+
+1. Fork (if not the author) and clone the repository
+2. `cd` in the directory and run `npm i`
+3. `npm install` the `peerDependencies` of this package if you will be building the code (to
+   publish, test, etc)
+4. Make your changes as required on a well named branch
+5. Open a PR against the origin repository
--- rollup.config.js
+++ rollup.config.js
@@ -4,13 +4,44 @@ import replace from '@rollup/plugin-replace'
 import { terser } from 'rollup-plugin-terser'
 
 export default [
+  // for unpkg with the all the dependencies rolled up Due to this
+  // setup, ?module on unpkg will not work as module key in
+  // package.json points to an esm module that doesn't have all the
+  // dependencies rolled up in it (e.g., graphql)
   {
     input: './src/index.js',
     output: [
       {
-        file: 'dist/serverless-gql.umd.js',
+        file: 'umd/serverless-gql.umd.js',
         format: 'umd',
         name: 'serverlessGql',
+        sourcemap: true
+      }
+    ],
+    // (node) resolve and the commonjs plugins let rollup resolve the
+    // dependencies (graphql and graphql/utilities) and include them
+    // in the bundle. This is useful only for the umd bundle served by
+    // unpkg which, when used, might not have access to graphql,
+    // etc. This does cause an issue where if the code that imports it
+    // via unpkg, if it also includes graphql, etc via unpkg, then
+    // graphql throws some weird errors as it detects two, potentially
+    // different version of graphql might be available thus causing
+    // weird behaviours during runtime and hence it throws.
+    plugins: [replace({ 'process.env.NODE_ENV': '"production"' }), resolve(), commonjs(), terser()]
+  },
+  {
+    // these bundle do NOT bundle up graphql and
+    // graphql/utilities. That is left to the consumer which is
+    // automatically done by the _application_ bundler. Here we only
+    // bundle the library for _consumption_ from an application. For
+    // the same reason, it doesn't minify these bundles as the
+    // application bundler will do so during consumption.
+    input: './src/index.js',
+    external: ['graphql', 'graphql/utilities'],
+    output: [
+      {
+        file: 'dist/serverless-gql.cjs.js',
+        format: 'cjs',
         sourcemap: true
       },
       {
@@ -19,6 +50,6 @@ export default [
         sourcemap: true
       }
     ],
-    plugins: [replace({ 'process.env.NODE_ENV': '"production"' }), resolve(), commonjs(), terser()]
+    plugins: [replace({ 'process.env.NODE_ENV': '"production"' })]
   }
 ]
