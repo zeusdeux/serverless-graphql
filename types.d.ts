be50701e26603a59da53eb673bdaaa2783574a14
Mudit Ameta <zeusdeux@gmail.com>
2019-12-29 03:39:49 +0100

    Add simple typescript type definitions

---
+++ types.d.ts
@@ -0,0 +1,28 @@
+import { GraphQLSchema } from 'graphql'
+
+export type ResolverFunction = (...args: any[]) => any
+
+export interface MakeExecutableSchemaOptions {
+  typeDefs: string
+  resolvers: {
+    [key: string]:
+      | {
+          [key: string]: ResolverFunction
+        }
+      | ResolverFunction
+  }
+}
+
+export interface QueryRunnerOptions {
+  req: string
+  variables?: { [key: string]: any }
+  root?: any
+  context?: any
+  operationToRun?: string
+}
+
+export type QueryRunnerFunction = (request: string | QueryRunnerFunction) => Promise<any>
+
+export function getQueryRunner(opts: MakeExecutableSchemaOptions): QueryRunnerFunction
+export function makeExecutableSchema(opts: MakeExecutableSchemaOptions): GraphQLSchema
+export const gql: typeof String.raw
